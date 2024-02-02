073a336c7d943204b577eaa24bf9bd0522ca4316
Fatih Yakutcan <145258989+FatihYcan@users.noreply.github.com>
2024-02-02 13:04:43 +0300

    Update Detail.jsx

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,145 +1,175 @@
 import * as React from "react";
-import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
-import { Button, Grid } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import { Box, Button, Container, List } from "@mui/material";
+import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
-import useBlogCalls from "../../hooks/useBlogCalls";
+import useBlogCalls from "../hooks/useBlogCalls";
+import CommentCard from "../components/blog/CommentCard";
+import CommentForm from "../components/blog/CommentForm";
+import UpdateModal from "../components/blog/UpdateModal";
+import DeleteModal from "../components/blog/DeleteModal";
 
-export default function BlogCard({
-  page,
-  _id,
-  id,
-  title,
-  content,
-  image,
-  comments,
-  likes,
-  countOfVisitors,
-  createdAt,
-}) {
-  const { userId } = useSelector((state) => state.auth);
-  const { postLikes, getBlogs } = useBlogCalls();
-  const navigate = useNavigate();
+export default function Detail() {
+  const { _id } = useParams();
+  const { details } = useSelector((state) => state.blog);
+  const { userId, username } = useSelector((state) => state.auth);
+  const { getDetails } = useBlogCalls();
 
-  const handleLike = async () => {
-    if (userId) {
-      await postLikes("blogs", _id);
-      await getBlogs(`/blogs?page=${page}&limit=10`);
-    } else {
-      navigate("/auth");
-    }
+  const [show, setShow] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
+  const [deleteOpen, setDeleteOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setData({
+      title: details.title,
+      image: details.image,
+      categoryId: details.categoryId._id,
+      isPublish: details.isPublish,
+      content: details.content,
+    });
+    setOpen(true);
   };
 
-  const handleDetail = () => {
-    navigate(`/detail/${_id}`);
-  };
+  const handleClose = () => setOpen(false);
+
+  const handleDeleteOpen = () => setDeleteOpen(true);
+
+  const { title, content, image, likes, comments, countOfVisitors } = details;
+
+  React.useEffect(() => {
+    getDetails({ id: _id });
+  }, []);
+
+  const [data, setData] = React.useState({
+    title: details.title,
+    image: details.image,
+    categoryId: details.categoryId,
+    isPublish: details.isPublish,
+    content: details.content,
+  });
+
+  const name = details.userId ? details.userId.username : "";
 
   return (
-    <Grid item key={_id} xs={12} md={6} lg={4} xl={3}>
-      <Card elevation={24} sx={{ margin: "10px" }}>
-        <Typography
-          variant="body1"
-          component="div"
-          sx={{ display: "flex", justifyContent: "center", height: "150px" }}
-        >
-          <CardMedia
-            component="img"
-            alt={title}
-            image={image}
-            sx={{
-              height: "175px",
-              width: "350px",
-              margin: "auto",
-              objectFit: "contain",
-              paddingTop: 2,
-            }}
-          />
-        </Typography>
-
-        <Typography
-          variant="body1"
-          component="div"
-          sx={{ width: "100%", height: "150px", marginTop: "8px" }}
-        >
-          <Typography
-            gutterBottom
-            variant="h6"
-            component="div"
-            sx={{
-              marginTop: "2rem",
-              textTransform: "uppercase",
-              textAlign: "center",
-              color: "darkblue",
-            }}
-          >
-            {title}
-          </Typography>
-          <Typography
-            variant="body2"
-            component="span"
-            style={{
-              display: "-webkit-box",
-              WebkitBoxOrient: "vertical",
-              overflow: "hidden",
-              WebkitLineClamp: 2,
-              margin: "16px",
-            }}
-          >
-            {content}
-          </Typography>
-          <Typography
-            gutterBottom
-            variant="body2"
-            component="span"
-            sx={{ textAlign: "left", margin: "16px" }}
-          >
-            Published Date : {createdAt && new Date(createdAt).toLocaleString()}
-          </Typography>
-        </Typography>
-
-        <Typography
-          variant="body1"
-          component="div"
+    <Container maxWidth="lg" sx={{ minHeight: "90vh" }}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "2rem",
+        }}
+      >
+        <Box
           sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            margin: "16px",
+            "@media (min-width:600px)": {
+              minWidth: "70%",
+              maxWidth: "70%",
+            },
+            marginTop: "3rem",
           }}
         >
-          <Typography variant="body1" component="div">
-            <IconButton
-              aria-label="add to favorites"
-              sx={{ textAlign: "left" }}
-              onClick={handleLike}
-            >
+          <CardMedia component="img" alt={title} image={image} />
+          <CardHeader
+            avatar={
+              <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe"></Avatar>
+            }
+            title={name}
+            subheader={
+              details.createdAt && new Date(details.createdAt).toLocaleString()
+            }
+          />
+
+          <CardContent>
+            <Typography component="h1" variant="body1">
+              {title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {content}
+            </Typography>
+          </CardContent>
+          <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites">
               {likes?.includes(userId) ? (
                 <FavoriteIcon color="error" />
               ) : (
                 <FavoriteIcon />
               )}
-              <span> {likes.length} </span>
+              <span> {likes?.length || 0} </span>
             </IconButton>
-            <IconButton aria-label="comments" sx={{ textAlign: "left" }}>
+            <IconButton aria-label="comments" onClick={() => setShow(!show)}>
               <ChatBubbleOutlineIcon />
-              <span> {comments.length}</span>
+              <span> {comments?.length || 0}</span>
             </IconButton>
-            <IconButton aria-label="view" sx={{ textAlign: "left" }}>
+            <IconButton aria-label="view">
               <VisibilityOutlinedIcon />
               <span> {countOfVisitors}</span>
             </IconButton>
-          </Typography>
-          <Button variant="contained" onClick={handleDetail}>
-            READ MORE
-          </Button>
-        </Typography>
-      </Card>
-    </Grid>
+          </CardActions>
+          {name === username && (
+            <Box my={2} display="flex" justifyContent="center">
+              <Button
+                variant="contained"
+                size="small"
+                sx={{ backgroundColor: "blue" }}
+                onClick={() => handleOpen()}
+              >
+                Update Blog
+              </Button>
+
+              <Button
+                variant="contained"
+                size="small"
+                color="error"
+                sx={{ marginLeft: "1rem" }}
+                onClick={handleDeleteOpen}
+              >
+                Delete Blog
+              </Button>
+            </Box>
+          )}
+
+          {show && (
+            <>
+              <CommentForm />
+              <Box>
+                <List
+                  sx={{
+                    width: "100%",
+                    maxWidth: 360,
+                    bgcolor: "background.paper",
+                  }}
+                >
+                  {comments?.map((item, i) => (
+                    <CommentCard key={i} {...item} />
+                  ))}
+                </List>
+              </Box>
+            </>
+          )}
+          <UpdateModal
+            open={open}
+            handleClose={handleClose}
+            data={data}
+            setData={setData}
+          />
+
+          <DeleteModal
+            open={deleteOpen}
+            handleClose={() => setDeleteOpen(false)}
+          />
+        </Box>
+      </Box>
+    </Container>
   );
 }
